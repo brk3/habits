77936e7a884c6310c4eff7be82119714bbeec949
Paul Bourke <pauldbourke@protonmail.com>
2025-07-30 21:33:39 +0100

    Rename log to track

--- cmd/track.go
+++ cmd/track.go
@@ -12,8 +12,9 @@ type Habit struct {
 	TimeStamp string
 }
 
-var logCmd = &cobra.Command{
-	Use:   "log",
+// trackCmd represents the track command
+var trackCmd = &cobra.Command{
+	Use:   "track",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,28 +22,26 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		log(args[0])
+		track(args[0])
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(logCmd)
+	rootCmd.AddCommand(trackCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// logCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// trackCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// logCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// trackCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-func log(content string) {
-	fmt.Println("log called!")
+func track(content string) {
 	h := &Habit{
 		Content:   content,
 		TimeStamp: "00:00:00",
