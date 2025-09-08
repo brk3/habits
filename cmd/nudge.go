2893a6ddd713ba753e123438ecc6b520dec54c0a
Paul Bourke <pauldbourke@protonmail.com>
2025-09-08 19:58:12 +0100

    Tidy up nudge config vars

--- cmd/nudge.go
+++ cmd/nudge.go
@@ -9,36 +9,28 @@ import (
 )
 
 var (
-	notifyEmail  string
-	resendApiKey string
+	notifyEmail    string
+	resendApiKey   string
+	nudgeThreshold int
 )
 
 var nudgeCmd = &cobra.Command{
 	Use:   "nudge",
 	Short: "Send a reminder for habit streaks expiring within a certain window",
 	PreRunE: func(cmd *cobra.Command, args []string) error {
-		if resendApiKey = os.Getenv("RESEND_API_KEY"); resendApiKey == "" {
+		if resendApiKey = os.Getenv("HABITS_RESEND_API_KEY"); resendApiKey == "" {
 			return fmt.Errorf("RESEND_API_KEY environment variable is not set")
 		}
-		if notifyEmail = os.Getenv("NOTIFY_EMAIL"); notifyEmail == "" {
+		if notifyEmail = os.Getenv("HABITS_NOTIFY_EMAIL"); notifyEmail == "" {
 			return fmt.Errorf("NOTIFY_EMAIL environment variable is not set")
 		}
-		if notifyHourBeforeMidnightEnv := os.Getenv("NOTIFY_HOUR_BEFORE_MIDNIGHT"); notifyHourBeforeMidnightEnv == "" {
-			return fmt.Errorf("NOTIFY_HOUR_BEFORE_MIDNIGHT environment variable is not set")
+		if nudgeThreshold := os.Getenv("HABITS_NUDGE_THRESHOLD"); nudgeThreshold == "" {
+			return fmt.Errorf("HABITS_NUDGE_THRESHOLD environment variable is not set")
 		}
-		// TODO(pbourke): finish rest of cfg vars
-		/*
-			if emailFlag == "" {
-				return fmt.Errorf("--email is required")
-			}
-			if hoursFlag <= 0 {
-				return fmt.Errorf("--hours must be greater than 0")
-			}
-		*/
 		return nil
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		nudge.Nudge("", 2, resendApiKey)
+		nudge.Nudge(notifyEmail, nudgeThreshold, resendApiKey)
 	},
 }
 
